c4257178347884f087d37def649294f01b0b8ee7
ivan gallego <ivan.gallego@vividseats.com>
2018-11-21 15:06:42 -0500

    Initial commits

--- src/test/java/money/CucumberConfig.java
+++ src/test/java/money/CucumberConfig.java
@@ -5,9 +5,8 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/java/resources/money",
-        glue= {"money"},
-        plugin = { "pretty","html:target/cucumber-reports"},
+@CucumberOptions(features = "src/test/java/resources/money/",
+        plugin = { "pretty","html:target/cucumber-html-reports","json:target/cucumber.json"},
         format = { "json:target/cucumber.json"},
         monochrome = true)
 public class CucumberConfig {
